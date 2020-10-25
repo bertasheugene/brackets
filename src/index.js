1a89ef05302d76cf90b9d1bfcb11322db0f75f34
Eugene3112 <bertoshevgieniy@gmail.com>
2020-10-25 23:04:18 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let combinations = [];
+  for(let i = 0; i < bracketsConfig.length; i++)
+  combinations.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  while (true)
+  {
+    if(str === '') return true;
+    let founded = -1;
+    for(let i = 0; i < combinations.length; i++ )
+    {
+      if(str.indexOf(combinations[i]) !== -1)
+      {
+        founded = i;
+        break;
+      }
+    }
+    if(founded === -1) return false;
+    str = str.replace(combinations[founded], '');
+  }
 }
